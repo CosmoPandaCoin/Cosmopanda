bf1ef5228844771522e1654881424fac3c782bde
CosmoPandaCoin <134861289+CosmoPandaCoin@users.noreply.github.com>
2023-05-31 01:16:23 +0100

    Update cosmopanda.sol

--- cosmopanda.sol
+++ cosmopanda.sol
@@ -2,36 +2,34 @@
 pragma solidity ^0.8.0;
 
 import "./ERC20.sol";
-
-contract CosmoPanda is ERC20 {
+import "./Ownable.sol";
+contract CosmoPanda is ERC20, Ownable {
    
     uint256 private _maxLimit;
-    address private _owner;
-    constructor(address devfund) 
-    ERC20("CosmoPanda", "COSMO") 
+    constructor(address devFund) 
+    ERC20("CosmoPanda", "PAND") 
     {
-        unchecked 
-        {
             _mint(msg.sender, 700000000 * (10 ** 18)); //70%
-            _mint(devfund, 300000000 * (10 ** 18)); //10% + 10% + 5% + 5%
-            _owner = msg.sender;       
-            _maxLimit = 10000000 * (10 ** 18); // Set initial maximum buy limit to 1% of total supply
-        }
+            _mint(devFund, 300000000 * (10 ** 18)); //10% + 10% + 5% + 5%     
+            _maxLimit = 10000000 * (10 ** 18); // Set initial maximum holding limit to 1% of total supply
     }
     function burn(uint256 value) external {
-        _burn(msg.sender, value);
+        _burn(_msgSender(), value);
     }
-    function setMaxLimit(uint256 limit) public 
+    function getMaxLimit() external onlyOwner view returns (uint256)
     {  
-        require(_owner == msg.sender, "Permission denied");
-        _maxLimit = limit * (10 ** 18); //account max token limit      
+        return _maxLimit; 
+    }
+    function setMaxLimit(uint256 limit) external onlyOwner 
+    {  
+         _maxLimit = limit * (10 ** 18); //account max token limit      
     }
     function _beforeTokenTransfer(
         address from,
         address to,
         uint256 amount
     ) internal override(ERC20) {
-        if (from != address(0) && to != address(0)) {
+        if (from != address(0) && to != address(0) && from != owner() && to != owner()) {
             require(
                 super.balanceOf(to) + amount <= _maxLimit,
                 "Exceeded maximum holding limit"
